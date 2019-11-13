857050d96e9da50922b8f7c45b425ee5403d5767
juan <jcbolanosq@gmail.com>
2019-11-13 12:49:47 -0800

    form with creating file data and retornar informacion

--- practice/express/requests/index.js
+++ practice/express/requests/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
@@ -16,17 +17,64 @@ app.use(bodyParser.urlencoded({extended: false }));
 
 
 // Express Routes
+// First Argument is the route name, second argument is directory to load when someone request the route name.
 app.use("/", express.static("./client"));
 
-app.post("/updateData", (request, response) => {
+// An http Post Handler called /submitComment
 
-    console.log(request.body.message);
+let filename = "commentHistory.json";
 
-    console.log("We recived a request for updateData");
 
-    let data = {text: "Thank you for your message"};
+app.post("/submitComment", (request, response) => {
 
-    response.send(data);
-    //response.sendStatus(200);
+    // Rename the data sent to us (located in request.body) to something simpler like objectFromRequest.
+    let objectFromRequest = request.body;
+
+    // console.log(objectFromRequest.message);
+    console.log(objectFromRequest);
+
+    //let text = objectFromRequest.message;
+    //console.log("We received a request for submitComment: " + text);
+
+ 
+    if ( fs.existsSync(filename)) {
+
+        let comments = fs.readFileSync(filename, "utf8");
+        comments = JSON.parse(comments);
+        comments.commentsArray.push(objectFromRequest);
+        comments = JSON.stringify(comments);
+        fs.writeFileSync(filename, comments, "utf8");
+        console.log("New comment saved to hard drive");
+    } else {
+        let comments = {commentsArray: [objectFromRequest]};
+        comments = JSON.stringify(comments);
+
+        fs.writeFileSync(filename, comments, "utf8");
+        console.log("Note: No save file detected, creating a new file");
+
+    }
+
+    // if you don't want to send any data bac, you can use. sendStatus(). You can only use sendStatus or send once to "fulfill" front-end request.
+    // response.sendStatus(200); //like return
+
+    // let data = {text: "Thank you for your message"};
+    // response.send(data);
+    
+    response.sendStatus(200);
+});
+
+app.post("/loadComments", (request, response) => {
+
+    if (fs.existsSync("commentHistory.json")) {
+
+        let comments = fs.readFileSync(filename, "utf8");
+        comments = JSON.parse(comments);
+
+        response.send(comments);
+    }else {
+        response.sendStatus(500);
+    }
+
+
 });
 
